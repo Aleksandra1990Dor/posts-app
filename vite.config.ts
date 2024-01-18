f42c3c15a1923e7fd4449fff6f81818ac64b0f9c
Aleksandra Dorofeeva <crazyleksy@gmail.com>
2024-01-18 23:07:14 +0300

    deploy commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: 'https://github.com/Aleksandra1990Dor/posts-app.git',
 	plugins: [react()],
 	css: {
 		postcss: {
