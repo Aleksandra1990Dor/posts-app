e4217e179569c675969444825a5531a4928d76fc
Aleksandra Dorofeeva <crazyleksy@gmail.com>
2024-01-18 23:36:20 +0300

    changed vite.config file v-2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: 'https://Aleksandra1990Dor/github.io/posts-app/',
+	base: '/posts-app',
 	plugins: [react()],
 	css: {
 		postcss: {
